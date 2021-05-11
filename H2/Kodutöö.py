ab7530b9fd8e6dd195cd73f215ae1f3a6b4b6f0e
Otger <otgervhl@gmail.com>
2021-05-11 12:07:42 +0300

    2 harjutuse tegemised.v1

---
+++ H2/Kodutöö.py
@@ -0,0 +1,15 @@
+class Kodutöö:
+    def __init__(self, pealkiri, hindeline, tehtud):
+        self.kodutööPealkiri = pealkiri
+        self.kodutööHindeline = hindeline
+        self.kodutööTehtud = tehtud
+
+
+
+
+
+
+
+
+
+
---
+++ H2/Õpilane.py
@@ -0,0 +1,17 @@
+class Õpilane:
+    def __init__(self, nimi, kursuseNimetus, tegemataKodutööd, tehtudKodutööd):
+        self.õpilaseNimi = nimi
+        self.õpilaseKursuseNimetus = kursuseNimetus
+        self.õpilsaeTegemataKodutööd = tegemataKodutööd
+        self.õpilsaeTehtudKodutööd = tehtudKodutööd
+
+    def lisaKodutöö(self):
+
+
+
+
+
+
+
+
+

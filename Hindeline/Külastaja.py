05486a4158e2a6b0ecb37a8bddea825304b103b3
Otger <otgervhl@gmail.com>
2021-05-11 14:30:04 +0300

    3 hindeline harjutuse tegemised.v1

---
+++ Hindeline/Külastaja.py
@@ -0,0 +1,32 @@
+from Raamat import Raamat
+
+class Külastaja:
+    def __init__(self, eesnimi, perekonnanimi):
+        self.külastajaEesnimi = eesnimi
+        self.külastajaPerekonnanimi = perekonnanimi
+        self.külastajaLaenatudRaamatud = []
+
+    def laenatudRaamat(self):
+        self.külastajaLaenatudRaamatud.append()
+
+
+
+    def tagastaRaamat(self):
+
+
+    def kuvaLaenatudRaamatud(self):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Hindeline/Raamat.py
@@ -0,0 +1,19 @@
+class Raamat:
+    def __init__(self, tiitel, autor, lehekülgedeArv):
+        self.raamatuTiitel = tiitel
+        self.raamatuAutor = autor
+        self.raamatuLehekülgedeArv = lehekülgedeArv
+
+
+
+
+
+
+
+
+
+
+
+
+
+
